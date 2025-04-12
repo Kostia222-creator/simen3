900bff25309074f7951a2ec24deeda69b97fdb26
Kostia222-creator <traftor222@gmail.com>
2025-04-12 12:24:41 +0000

    сим с рутом добавить библиотеку

--- pineapple.h
+++ pineapple.h
@@ -3,5 +3,5 @@
 double Plus(double N1, double N2);
 double Minus(double N1, double N2);
 double Umnog(double N1, double N2);
-double Delenie(double N1, double N2)
+double Delenie(double N1, double N2);
 #endif
